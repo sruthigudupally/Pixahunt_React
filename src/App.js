4c09584605634dc0470091c8317d0e1521f825c1
Sruthi Reddy Gudupally <sruthigudupally@gmail.com>
2024-10-06 13:55:46 +0530

    welcome

--- src/App.js
+++ src/App.js
@@ -18,8 +18,9 @@ import './style.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Pixabay from './components/Pixabay.jsx';
 import './App.css';
-import Loginpage from '../src/components/pages/Loginpage.jsx'
-import Registrationpage from '../src/components/pages/Registrationpage.jsx'
+import Loginpage from './pages/Loginpage.jsx'
+import Registrationpage from './pages/Registrationpage.jsx'
+import Error from './pages/Errorpage.jsx'
 export default class App extends React.Component{
   constructor(props){
     super(props)
@@ -32,8 +33,10 @@ export default class App extends React.Component{
         <BrowserRouter>
       <Routes>
       <Route path="/" element={<Pixabay/>}/>
+     
       <Route path="/login" element={<Loginpage/>}/>
       <Route path="/register"   element={<Registrationpage/>}/>
+      <Route path="*" element={<Error/>}/>
           {/*<Route path="/goa" element={<Goa/>}></Route>
            <Route path="/perth" element={<PerthbyGoa/>}></Route> */}
       </Routes>
